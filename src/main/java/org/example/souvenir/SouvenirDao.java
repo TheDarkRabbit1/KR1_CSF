9b50afa27e0f76ba789eea45ada7c7865fbb2c28
TDR <darkrabitthe@gmail.com>
2023-05-07 13:08:19 +0300

    SouvenirDao+souvenirService

--- src/main/java/org/example/souvenir/SouvenirDao.java
+++ src/main/java/org/example/souvenir/SouvenirDao.java
@@ -1,4 +1,45 @@
 package org.example.souvenir;
 
+
+import lombok.SneakyThrows;
+
+import java.io.*;
+import java.util.Collections;
+import java.util.List;
+
 public class SouvenirDao {
+    public static SouvenirDao instance;
+    public static List<Souvenir> souvenirs;
+
+    public static SouvenirDao getInstance() {
+        instance = instance != null ? instance : new SouvenirDao();
+        return instance;
+    }
+    public List<Souvenir> getSouvenirs(){
+        return souvenirs;
+    }
+    public void addSouvenir(Souvenir souvenir){
+        souvenirs.add(souvenir);
+    }
+    public boolean removeSouvenir(Souvenir souvenir){
+        return souvenirs.removeIf(s->s.name.equals(souvenir.name)&&s.props.equals(souvenir.props));
+    }
+    @SneakyThrows
+    public SouvenirDao() {
+        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream("souvenir.sv"))) {
+            souvenirs = (List<Souvenir>) in.readObject();
+        } catch (FileNotFoundException e) {
+            souvenirs = Collections.emptyList();
+        }
+    }
+
+    @SneakyThrows
+    public static void close() {
+        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("souvenir.sv"))) {
+            if (souvenirs==null)
+                souvenirs=Collections.emptyList();
+            out.writeObject(souvenirs);
+        }
+    }
+
 }
--- src/main/java/org/example/souvenir/SouvenirService.java
+++ src/main/java/org/example/souvenir/SouvenirService.java
@@ -1,4 +1,26 @@
 package org.example.souvenir;
 
+import org.example.manufacturer.ManufacturerProps;
+
+import java.util.List;
+
 public class SouvenirService {
+    private static SouvenirDao souvenirDao = SouvenirDao.getInstance();
+    private static SouvenirService instance;
+    private static SouvenirService getInstance(){
+        instance=instance!=null?instance:new SouvenirService();
+        return instance;
+    }
+    public List<Souvenir> getSouvenirs(){
+        return souvenirDao.getSouvenirs();
+    }
+    public List<Souvenir> getSouvenirsByProps(ManufacturerProps manufacturerProps){
+        return souvenirDao.getSouvenirs().stream()
+                .filter(s->!s.props.equals(manufacturerProps))
+                .toList();
+    }
+    public void removeSouvenir(Souvenir souvenir){
+        if (!souvenirDao.removeSouvenir(souvenir))
+            System.out.println("no such souvenir to remove");
+    }
 }
